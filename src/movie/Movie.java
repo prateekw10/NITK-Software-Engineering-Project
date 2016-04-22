07d93d791517c45e5ca44cf6ee513908524b98f3
Prateek Wadhwani <prateek.w10@gmail.com>
2016-04-22 22:07:30 +0530

    Added files via upload

---
+++ src/movie/Movie.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movie;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Prateek
+ */
+public class Movie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Entered movie function");
+        JFrame op = new NewJFrame();
+        op.setLocationRelativeTo(null);
+        op.setVisible(true);
+    }
+    
+}
